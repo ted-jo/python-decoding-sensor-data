8e3cfb6bd2157c51b6d5221a5e70d70996a724dd
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 00:19:09 -0500

    Created sensor drivers

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,6 +6,7 @@ from humidity_info import HumidityData
 from particle_count_info import ParticleData
 from temperature_info import TemperatureData
 from statistics import mean
+from energy_info import EnergyData
 
 ##############################
 # Do not remove these two lines
@@ -61,6 +62,15 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
-
 # Module 5 code here:
+
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
 
