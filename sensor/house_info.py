88dcf1993235d61b08e89417de7f502ad9988dff
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 22:12:02 -0500

    Fixed sensoru_app function

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,10 +1,10 @@
 from datetime import date, datetime
 
-class HouseInfo:
+class HouseInfo(object):
     def __init__(self, data):
         self.data = data
     
-    def get_data_by_area(self, field, rec_area = 0):
+    def get_data_by_area(self, field, rec_area=0):
         field_data = []
         for record in self.data:
             if rec_area == 0:
@@ -13,9 +13,10 @@ class HouseInfo:
                 field_data.append(record[field])
         return field_data
     
-    def get_data_by_date(self, field, rec_date = date.today()):
+    def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
+
         for record in self.data:
             if rec_date.strftime("%m/%d/%y") == record['date']:
                 field_data.append(record[field])
-        return field_data
+        return field_data 
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -18,11 +18,12 @@ print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
-recs = house_info.get_data_by_area(id, rec_area=test_area)
+recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
-print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
 
