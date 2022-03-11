12786bd9008eaf8247b428694bf4d310fad387c0
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 22:44:45 -0500

    get_data_by_date Override function created

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -1,4 +1,5 @@
 from house_info import HouseInfo
+from datetime import date
 
 class TemperatureData(HouseInfo):
     def _convert_data(self, data):
@@ -6,3 +7,11 @@ class TemperatureData(HouseInfo):
         for rec in data:
             recs.append(int(rec,base=10))
         return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("temperature", rec_area)
+        return self._convert_data(recs)
+    
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("temperature", rec_date)
+        return self._convert_data(recs)
