5896d52f8ae91de5890ebb76ae36b5b9ce4d557e
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 21:32:43 -0500

    get_data_by_area function created

---
+++ sensor/house_info.py
@@ -0,0 +1,12 @@
+class HouseInfo:
+    def __init__(self, data):
+        data = self.data
+    
+    def get_data_by_area(self, field, rec_area = 0):
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == recor['area']:
+                field_data.append(record[field])
+        return field_data
